94b77e222e19860d64905e4b908402af19cd64c7
Steve Burke <1440497+steveburke@users.noreply.github.com>
2024-04-29 15:18:26 -0400

    added a better delete confirmation

--- library-management-ui/src/app/bookedit/bookedit.component.ts
+++ library-management-ui/src/app/bookedit/bookedit.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { Book } from '../book';
 import { LibraryService } from '../library.service';
 import { SecurityService } from '../security.service';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -17,6 +16,7 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class BookeditComponent {
   book: any;
   isLoggedIn:boolean = false;
+  private modalService = inject(NgbModal);
 
   constructor(public libraryService: LibraryService, 
       private securityService: SecurityService,
@@ -26,7 +26,7 @@ export class BookeditComponent {
       this.book = Object.assign({}, navigation.extras.state?.['book']);
     }
    }
-
+  
   saveBook(){
     this.libraryService.updateBook(this.book).subscribe((result) => {
       this.book = result.data.updateBook;
@@ -34,17 +34,54 @@ export class BookeditComponent {
     })
   }
 
-  deleteBook(){
-    this.libraryService.deleteBook(this.book.id).subscribe((result) => {
-      this.router.navigate(['booklist']);
-    });
+  openConfirmationModal(){
+    this.modalService.open(NgbdModalConfirm);
   }
-
   ngOnInit(): void{
     this.isLoggedIn = this.securityService.isLoggedIn();
   }
 
   cancel(){
     this.router.navigate(['booklist']);
+  }
+}
+
+@Component({
+	selector: 'ngbd-modal-confirm',
+	standalone: true,
+	template: `
+		<div class="modal-header">
+			<h4 class="modal-title" id="modal-title">Are you sure?</h4>
+			<button
+				type="button"
+				class="btn-close"
+				aria-describedby="modal-title"
+				(click)="modal.dismiss('Cross click')"
+			></button>
+		</div>
+		<div class="modal-body">
+			<p>
+				<strong>Are you sure you want to delete this book?</strong>
+			</p>
+			<p>
+				<span class="text-danger">This operation can not be undone.</span>
+			</p>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">Cancel</button>
+			<button type="button" class="btn btn-danger" (click)="deleteBook()">Ok</button>
+		</div>
+	`,
+})
+
+export class NgbdModalConfirm {
+	modal = inject(NgbActiveModal);
+  constructor(public libraryService: LibraryService){
+
+  }
+  deleteBook(){
+    this.libraryService.deleteBook(this.book.id).subscribe((result) => {
+      this.modal.close;
+    });
   }
 }
